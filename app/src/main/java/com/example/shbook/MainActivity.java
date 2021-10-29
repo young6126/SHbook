8dba9f80495ed3dcba7b1ac8bf393159efa6dd49
oiqwq <oiqwqi@gachon.ac.kr>
2021-10-29 20:11:54 +0900

    2021-10-29 barcode

--- app/src/main/java/com/example/shbook/MainActivity.java
+++ app/src/main/java/com/example/shbook/MainActivity.java
@@ -2,15 +2,35 @@ package com.example.shbook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.zxing.client.android.Intents;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSell;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnSell = (Button)findViewById(R.id.button2);
 
+        btnSell.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SellMain.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
